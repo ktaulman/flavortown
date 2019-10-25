2ef4e1e30dd1fb43ce2384025dcae1effdbb44e0
Kevin Taulman <kevin.taulman@icloud.com>
2019-10-24 20:00:38 -0400

    heroku change

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ export default function App (){
         <SearchBar /> 
       </Header>
       
-      {!isGuyClicked?
+      {isGuyClicked?
       <div>
       <BannerAd src="http://realpants.com/wp-content/uploads/2015/03/648x300xpromo-guy-fieri-savory-sausages.jpg.pagespeed.ic_.5TFvSsrJ8x.jpg" />
         <Random />
